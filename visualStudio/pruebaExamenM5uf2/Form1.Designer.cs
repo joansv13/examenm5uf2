e8b8a30cac1dc3d5660e3566da2ed1e2ab65d6ad
joansv13 <91868339+joansv13@users.noreply.github.com>
2023-03-20 11:53:44 +0100

    quart commit

--- visualStudio/pruebaExamenM5uf2/Form1.Designer.cs
+++ visualStudio/pruebaExamenM5uf2/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace pruebaExamenM5uf2
 {
-    partial class Form1
+    partial class FrmMain
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -28,13 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Yellow;
+            this.button1.Location = new System.Drawing.Point(35, 33);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(279, 128);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Groc";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.button2.Location = new System.Drawing.Point(334, 33);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(279, 128);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Verd";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "FrmMain";
+            this.Text = "FrmMain";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
